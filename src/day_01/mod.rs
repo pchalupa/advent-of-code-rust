99a0d53896147b0cd1c59b2398ac356e4ff4b633
pchalupa <chalupa.petr93@gmail.com>
2024-12-02 10:24:50 +0100

    solution for day 1

---
+++ src/day_01/mod.rs
@@ -0,0 +1,105 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use regex::Regex;
+
+use crate::utils::{print_result, read_input_as_buffer};
+
+pub fn main() {
+    let reader = read_input_as_buffer(1, false);
+    let (left_list, right_list) = preprocess(reader);
+    let result_1 = task_1(&left_list, &right_list);
+    let result_2 = task_2(&left_list, &right_list);
+
+    print_result(result_1, result_2);
+}
+
+fn preprocess(reader: BufReader<File>) -> (Vec<u32>, Vec<u32>) {
+    let pattern = Regex::new(r"(\d+)\s+(\d+)").expect("Invalid regex");
+    let mut left_list: Vec<u32> = Vec::new();
+    let mut right_list: Vec<u32> = Vec::new();
+
+    for line in reader.lines() {
+        let foo = line.expect("Unable to read a line");
+        let captures = pattern.captures(&foo).expect("Regex not matched");
+
+        let left_value: u32 = captures
+            .get(1)
+            .expect("Unable to find the first number")
+            .as_str()
+            .parse::<u32>()
+            .expect("Invalid number");
+        let right_value: u32 = captures
+            .get(2)
+            .expect("Unable to find the second number")
+            .as_str()
+            .parse::<u32>()
+            .expect("Invlaid number");
+
+        left_list.push(left_value);
+        right_list.push(right_value);
+    }
+
+    left_list.sort();
+    right_list.sort();
+
+    return (left_list, right_list);
+}
+
+fn task_1(left_list: &Vec<u32>, right_list: &Vec<u32>) -> u32 {
+    let mut result: u32 = 0;
+    let mut i = 0;
+
+    while i < left_list.len() {
+        let &left_value = left_list.get(i).expect("Not able to get value");
+        let &right_value = right_list.get(i).expect("Not able to get value");
+
+        result += left_value.abs_diff(right_value);
+
+        i += 1;
+    }
+
+    return result;
+}
+
+fn task_2(left_list: &Vec<u32>, right_list: &Vec<u32>) -> u32 {
+    let mut result: u32 = 0;
+
+    for &value in left_list {
+        let count: u32 = right_list
+            .iter()
+            .filter(|&&x| x == value)
+            .count()
+            .try_into()
+            .expect("Not able to convert");
+
+        result += value * count
+    }
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task_1() {
+        let reader = read_input_as_buffer(1, true);
+        let (left_list, right_list) = preprocess(reader);
+        let result = task_1(&left_list, &right_list);
+
+        assert_eq!(result, 11)
+    }
+
+    #[test]
+    fn test_task_2() {
+        let reader = read_input_as_buffer(1, true);
+        let (left_list, right_list) = preprocess(reader);
+        let result = task_2(&left_list, &right_list);
+
+        assert_eq!(result, 31)
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,22 @@
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    path::Path,
+};
+
+pub fn read_input_as_buffer(day: u8, test: bool) -> BufReader<File> {
+    let file_name: String = if test {
+        format!("input/{day}_test.txt")
+    } else {
+        format!("input/{day}.txt")
+    };
+    let path = Path::new(&file_name);
+    let file = File::open(path).expect("Failed to open the file!");
+    let reader = io::BufReader::new(file);
+
+    return reader;
+}
+
+pub fn print_result(task_1: u32, task_2: u32) {
+    println!("Task 1: {}\nTask 2: {}", task_1, task_2)
+}
